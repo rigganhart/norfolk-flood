67b0c2642fc598b7af01733e1e01139089deb2e4
Riggan Hart <rigganh@gmail.com>
2016-12-02 17:01:14 -0500

    adding express and mongodb to server requires

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 var http = require('http');
+var express = require('express');
+var mongoDB = require('mongoDB');
+
+
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/plain'});
 	res.end('Hello World\n');
