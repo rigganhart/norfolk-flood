4304f61e3cd8540e08d82248f25a7596aa45845b
Jason Wilson <jason@wilsons.io>
2016-12-02 22:53:57 -0500

    index.html: controller updates, app.js: added logic for income submission

--- public/app.js
+++ public/app.js
@@ -4,8 +4,24 @@ let app = angular.module('Norfolk-Flood',[]);
 // User Input Controller:
 app.controller('userIncomeController', ['$scope','$http', 'dataService', function($scope,$http, dataService){
 
-  $scope.simulatedData = dataService.getSimData();
+  $scope.simulatedData = dataService.getSimData(); //income, response
   $scope.realData = dataService.getRealData();
+  $scope.rangeData = dataService.getIncomeRanges();
+  
+  $scope.getIncomeImpact = function() {
+    //Identify income range and calculate flood impact values
+    
+    var incomeRange = $scope.rangeData.filter( function(range) {
+      $scope.incomeLevel >= range.Min && $scope.incomeLevel <= range.MaxUpTo; 
+    });
+    
+    $scope.flood = {
+      level: '0.0',
+      levelName: 'NONE',
+      cost: $scope.incomeLevel * incomeRange['0m'],
+      message: 'If the sea level stays the same, you can expect to lose $' + parseFloat($scope.incomeLevel * incomeRange['0m']).toFixed(0)
+    }
+  }
 
 
 
@@ -16,6 +32,16 @@ app.factory('dataService', ['$http',function($http){
   var realData = [];
 
   return{
+    getIncomeRanges : function(){
+      $http({
+        method: 'GET',
+        url: '/ranges'
+      }).then(function(response){
+        console.log("income ranges from server",response);
+        angular.copy(response.data,rangeData)
+      })
+      return simData;
+    },
     getSimData : function(){
       $http({
         method: 'GET',
